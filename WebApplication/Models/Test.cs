8f5b375aa5e2de5ca91327225527dba1ed6c9fdb
othman <d_misane@hotmail.com>
2018-03-17 14:02:37 +0100

    Creation et parametrisation de l'application web

---
+++ WebApplication/Models/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wrapper;
+
+namespace WebApplication.Models
+{
+    public class Test
+    {
+        public double displayPrice()
+        {
+            WrapperClass wc = new WrapperClass();
+            return wc.getPrice();
+
+        }
+    }
+}
