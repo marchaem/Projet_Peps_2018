65262c51bd75028d1ed88f0af290517a2191e871
othman <d_misane@hotmail.com>
2018-03-19 23:16:06 +0100

    Interface Informations

---
+++ WebApplication/Controllers/InformationsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class InformationsController : Controller
+    {
+
+        static Informations infos;
+
+        // GET: Informations
+        public ActionResult Index()
+        {
+            infos = new Informations();
+            return View();
+        }
+
+        //
+        // POST: /Informations/ 
+        [HttpPost]
+        public String Index(Informations info)
+        {
+            DateTime dateDebut = new DateTime(2014,12,18);
+            double diff= (info.DateCourante - dateDebut).TotalDays / 365;
+            infos.t = diff;
+            if (ModelState.IsValid)
+                return "la date est " + infos.t.ToString();
+            // return RedirectToAction("Index", "Accueil");
+
+
+            // If we got this far, something failed, redisplay form
+            return "hello";
+           // return View();
+        }
+    }
+}
---
+++ WebApplication/Models/Informations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Informations
+    {
+        public double t;
+
+        [Required(ErrorMessage = "La date courante est obligatoire")]
+        [DataType(DataType.Date)]
+        public DateTime DateCourante { get; set; }
+    }
+}
