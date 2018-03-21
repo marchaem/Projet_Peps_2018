5dc701187e1ebea4057e1c8fe199be285272f729
benjellt <thami.benjelloun@grenoble-inp.org>
2018-03-21 09:40:49 +0100

    Bonne valeur au départ

--- WebApplication/Models/Test.cs
+++ WebApplication/Models/Test.cs
@@ -15,32 +15,37 @@ namespace WebApplication.Models
 
 
             //int size, double r, double* VarHis, double* spot, double* trend, double fdStep, int nbSamples, double strike, double T1, int nbTimeSteps1, double* lambdas1
-
+            double r_eu = 0.002;
+            double r_aus = 0.025;
+            double r_us = 0.00025;
             int size = 5;
-            double r = 0.01;
+            double r = r_eu;
             double[,] varHis = new double[5,5];
             for (int i = 0; i < 5; i++)
             {
                 for(int j=0; j<5; j++)
                 {
-                    varHis[i,j] = 0.15;
+                    varHis[i,j] = 0.2;
                 }
                 
             }
 
             double[] spots = new double[5];
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
                 spots[i] = 100;
             }
+            spots[3] = 1.0;
+            spots[4] = 1.0;
 
             
 
             double[] trends = new double[5];
-            for (int i = 0; i < 5; i++)
-            {
-                trends[i] = 0.05;
-            }
+            trends[0] = r_eu;
+            trends[1] = r_us - 0.1 * 0.2 * 0.2;
+            trends[2] = r_aus - 0.1 * 0.2 * 0.2;
+            trends[3] = r_eu - r_us;
+            trends[4] = r_eu - r_aus;
 
             double[] lambdas = new double[5];
             for (int i = 0; i < 5; i++)
