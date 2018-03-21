283071c0e76e50857340f423b74b1751d7fd15d2
elrhatrif <aminelrhatrif@gmail.com>
2018-03-21 08:28:44 +0100

    Pull

--- Wrapper/Wrapper.cpp
+++ Wrapper/Wrapper.cpp
@@ -15,14 +15,13 @@ namespace Wrapper {
 	
 	}
 
-	cli::array<double>^ WrapperClass::getDeltaEurostral(cli::array<double,2>^ past,double t) {
+	/*cli::array<double>^ WrapperClass::getDeltaEurostral(cli::array<double,2>^ past,double t) {
 		double * convert_past = convertMatrixPointer(past);
 		PnlVect * delta=lien->deltaEurostral(convert_past,t);
 		return(convertPnlVectToCli(delta));
-	}
+	}*/
 
 	WrapperClass::WrapperClass(int size, double r, cli::array<double,2>^ VarHis, cli::array<double>^ spot, cli::array<double>^ trend, double fdStep, int nbSamples, double strike, double T1, int nbTimeSteps1, cli::array<double>^ lambdas1) {
-		pricer1 = new Pricer();
 		double * convert_varHis = convertMatrixPointer(VarHis);
 		double * convert_spot = convertArrayPointer(spot);
 		double * convert_trend = convertArrayPointer(trend);
@@ -57,12 +56,12 @@ namespace Wrapper {
 		return res;
 	}
 
-	cli::array<double>^ WrapperClass::convertPnlVectToCli(PnlVect * delta) {
+	/*cli::array<double>^ WrapperClass::convertPnlVectToCli(PnlVect * delta) {
 		cli::array<double>^ delta_cli = gcnew cli::array<double,1>(delta->size);
 		for (int i = 0; i < delta->size - 1; i++) {
 			delta_cli[i] = pnl_vect_get(delta, i);
 		}
 		return delta_cli;
-	}
+	}*/
 }
 
--- Wrapper/Wrapper.h
+++ Wrapper/Wrapper.h
@@ -17,11 +17,11 @@ namespace Wrapper {
 		WrapperClass::WrapperClass();
 	    WrapperClass::WrapperClass(int size, double r, cli::array<double,2>^ VarHis, cli::array<double>^ spot, cli::array<double>^ trend, double fdStep, int nbSamples, double strike, double T1, int nbTimeSteps1, cli::array<double>^ lambdas1);
 		double getPriceEurostral();
-		cli::array<double>^ getDeltaEurostral(cli::array<double, 2>^ past, double t);
+		//cli::array<double>^ getDeltaEurostral(cli::array<double, 2>^ past, double t);
 		double getZero();
 		double* convertArrayPointer(cli::array<double> ^ arr);
 		double* convertMatrixPointer(cli::array<double,2> ^ mat);
-		cli::array<double>^ convertPnlVectToCli(PnlVect * delta);
+		//cli::array<double>^ convertPnlVectToCli(PnlVect * delta);
 		
 	};
 }
