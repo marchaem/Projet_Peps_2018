74da7cefe6526a1777e08f512fe9a01800397f83
othman <d_misane@hotmail.com>
2018-03-22 11:06:14 +0100

    Modèle et vue de la nouvelle interface

---
+++ WebEurostral/Models/EurostralModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Wrapper;
+
+namespace WebEurostral.Models
+{ 
+    public class EurostralModels
+    {
+        public ParametresModel p;
+        public double t;
+        public WrapperClass wc;
+        public double prixEnt;
+
+        public EurostralModels()
+        {
+            p = new ParametresModel();
+            WrapperClass wc = new WrapperClass();
+        }
+
+        public void Pricet(double t,double[,] past)
+        {
+            prixEnt=wc.getPriceEurostral(t, past);
+        }
+    }
+}
