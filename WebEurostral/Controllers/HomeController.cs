59f7404e9413d45b32c4947d27ad5babe2221960
othman <d_misane@hotmail.com>
2018-03-22 14:39:47 +0100

    nouvelle interface à ne pas utiliser pour l'instant

--- WebEurostral/Controllers/HomeController.cs
+++ WebEurostral/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebEurostral.Models;
+using Data;
 
 namespace WebEurostral.Controllers
 {
@@ -13,26 +14,28 @@ namespace WebEurostral.Controllers
         // GET: Parametres
         public ActionResult Index()
         {
-            return View();
+            //ParametresModel param = new ParametresModel();
+            return View();//param);
         }
 
         // POST: /Parametres/ 
         [HttpPost]
-        public ActionResult Index(EurostralModels eurostP)//(Test tests)
+        public String Index(EurostralModels eurost)//(Test tests)
         {
-            
+            //eurost.p = param2;
             DateTime dateDebut = new DateTime(2014, 12, 18);
-            double diff = (eurostP.p.DateCourante - dateDebut).TotalDays / 365;
-            eurostP.t = diff;
+            //double diff = (tests.DateCourante - dateDebut).TotalDays / 365;
+            double diff = (eurost.DateCourante-dateDebut).TotalDays / 365.0;
+            eurost.t = diff;
             //if (ModelState.IsValid
             //ViewData["Delta"] = tests.displayDelta0(tests.DateCourante);
-            //return "le t est de " + (eurost.t).ToString(); 
+            return "le t est de " + (eurost.t).ToString(); 
             //return RedirectToAction("Index");
             
 
             // If we got this far, something failed, redisplay form
             //return "hello";
-             return RedirectToAction("PrixEnt");
+            // return RedirectToAction("PrixEnt");
         }
 
         public ActionResult About()
@@ -46,7 +49,25 @@ namespace WebEurostral.Controllers
         {
             //ViewBag.Message = "Your contact page.";
             int nbActions = 3;
-            double[,] past = new double[,nbActions];
+
+            double[,] past = new double[2,nbActions];
+
+            DateTime date = new DateTime(2014, 12, 18);
+            DateTime debutProduit = new DateTime(2014, 12, 18);
+            DateTime finProduit = new DateTime(2022, 12, 08);
+            Data.RecupData recup = new RecupData(new DateTime(2000, 1, 1), date);
+
+           
+            recup.Fetch();
+            List<string> symbols = recup.getSymbols();
+            Dictionary<DateTime,double> dico = recup.getData(symbols[0]);
+
+
+
+            double[,] covLogR = recup.exportCov();
+            double[,] pastDelta = recup.exportPast(t, 182, debutProduit, finProduit);
+            double[,] pastPrice = recup.exportPast(t, 182, debutProduit, finProduit);
+
             eurost.Pricet(eurost.t, past); // Il faut initialiser t dans Eurostral !!!!!
 
             return View();
---
+++ WebEurostral/Global.asax.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebEurostral
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            String _path = String.Concat(System.Environment.GetEnvironmentVariable("PATH"), ";", System.AppDomain.CurrentDomain.RelativeSearchPath);
+            System.Environment.SetEnvironmentVariable("PATH", _path, EnvironmentVariableTarget.Process);
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
