8bb85c71c648415bb278ae7fa5cc9e5110e47c8f
othman <d_misane@hotmail.com>
2018-03-29 15:08:34 +0200

    interface

--- WebEurostral/Controllers/HomeController.cs
+++ WebEurostral/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace WebEurostral.Controllers
         {
             eurost = eurost2;
             DateTime dateDebut = new DateTime(2014, 12, 18);
-            //double diff = (tests.DateCourante - dateDebut).TotalDays / 365;
+        
             double diff = (eurost.DateCourante-dateDebut).TotalDays / 365.0;
             eurost.t = diff;
             //if (ModelState.IsValid
@@ -40,8 +40,7 @@ namespace WebEurostral.Controllers
             return RedirectToAction("DeltaEnt");
             
 
-            // If we got this far, something failed, redisplay form
-            //return "hello";
+          
             // return RedirectToAction("PrixEnt");
         }
 
@@ -54,65 +53,11 @@ namespace WebEurostral.Controllers
 
         public ActionResult PrixEnt()
         {
-            //ViewBag.Message = "Your contact page.";
-           /* int nbActions = 3;
-
-            double[,] past = new double[2,nbActions];
-
-            DateTime date = new DateTime(2014, 12, 18);
-            DateTime debutProduit = new DateTime(2014, 12, 18);
-            DateTime finProduit = new DateTime(2022, 12, 18);
-            Data.RecupData recup = new RecupData(new DateTime(2000, 1, 1), date);
-
            
-            recup.Fetch();
-            List<string> symbols = recup.getSymbols();
-            Dictionary<DateTime,double> dico = recup.getData(symbols[0]);
-
-
-            eurost.covLogR = new double[5,5];
-            eurost.pastDelta= new double[5, 5];
-            eurost.pastPrice = new double[5, 5];
-            eurost.covLogR = recup.exportCov();
-            eurost.pastDelta = recup.exportPast(eurost.t, 182, debutProduit, finProduit);
-            eurost.pastPrice = recup.exportPast(eurost.t, 182, debutProduit, finProduit);*/
-
-          /*  double r_eu = 0.002;
-            double r_aus = 0.025;
-            double r_us = 0.00025;
-            int size = 5;
-            double r = r_eu;
-
-
-
-            double[] spots = new double[5];
-            for (int i = 0; i < 5; i++)
-            {
-                spots[i] = eurost.pastPrice[0, i];
-            }
-
-
-
-            double[] trends = new double[5];
-            trends[0] = r_eu;
-            trends[1] = r_us - eurost.covLogR[1, 3];
-            trends[2] = r_aus - eurost.covLogR[2, 4];
-            trends[3] = r_eu - r_us;
-            trends[4] = r_eu - r_aus;
-
-            double[] lambdas = new double[5];
-            for (int i = 0; i < 5; i++)
-            {
-                lambdas[i] = 0.05;
-            }
-
-            WrapperClass wr1 = new WrapperClass(size, r, eurost.covLogR, spots, trends, 0.1, eurost.nb_iterations, 100, 8.0, 15, lambdas);*/
            
             eurost.prixEnt=eurost.wc.getPriceEurostral(eurost.t, eurost.pastPrice); // Il faut initialiser t dans Eurostral !!!!!
-            //wr1.getPriceEurostral(eurost.t, pastPrice);
-           // return (eurost.prixEnt);
 
-            ViewData["price"] = eurost.prixEnt;
+            
             return View(eurost);
             //return RedirectToAction("DeltaEnt");
         }
@@ -186,7 +131,7 @@ namespace WebEurostral.Controllers
                 Dictionary<DateTime, double> dico = recup.getData(symbols[0]);
 
 
-                eurost.covLogR = recup.exportCov(new DateTime(2010, 12,18 ), debutProduit);
+                eurost.covLogR = recup.exportCov(new DateTime(2004, 12,18 ), debutProduit);
                 eurost.pastDelta = recup.exportPast(eurost.t, 7, debutProduit, finProduit);
                 eurost.pastPrice = recup.exportPast(eurost.t, 182, debutProduit, finProduit);
 
